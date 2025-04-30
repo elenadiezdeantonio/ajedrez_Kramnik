9f6c1e1b84f081274c69cf947d90a166907488e8
carlos <carlosdlb.1c@gmail.com>
2025-04-30 13:38:29 +0200

    Creamos la clase hija pieza rey

---
+++ rey.cpp
@@ -0,0 +1,16 @@
+#include "rey.h"
+#include <cmath>
+Rey::Rey(Color color, Posicion pos) : Pieza(color, pos) {}
+char Rey::getSimbolo()const {
+	if (color == Color::BLANCO) {
+		return 'R';
+	}
+	else {
+		return 'r';
+	}
+}
+bool Rey::esMovimientoValido(Posicion destino, const Tablero& tablero) const {
+	int df = abs(destino.fila - posicion.fila);
+	int dc = abs(destino.columna - posicion.columna);
+	return (df <= 1 && dc <= 1) && !(df == 0 && dc == 0);
+}
