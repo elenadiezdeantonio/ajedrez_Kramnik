ac9508ad8fd0528363c6def650a5c891aaeb68ac
sara.aparicio <sara.aparicio@alumnos.upm.es>
2025-05-07 19:26:42 +0200

    Creación pieza Alfil

---
+++ alfil.cpp
@@ -0,0 +1,20 @@
+#include "alfil.h"
+#include <cmath>
+Alfil::Alfil(Color color, Posicion pos) : Pieza(color, pos) {}
+
+char Alfil::getSimbolo() const {
+    if (color == Color::BLANCO) {
+        return 'A';
+    }
+    else {
+        return 'a';
+    }
+}
+
+bool Alfil::esMovimientoValido(Posicion destino, const Tablero& tablero) const {
+    // Movimiento en diagonal:la fila y columna siempre aumentan igual.
+    int df = std::abs(destino.fila - posicion.fila);
+    int dc = std::abs(destino.columna - posicion.columna);
+    return (destino != posicion) && (df == dc);
+
+}
---
+++ alfil.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "pieza.h"
+class Alfil :public Pieza
+{
+public:
+	Alfil(Color color, Posicion pos);
+	char getSimbolo()const override;
+	bool esMovimientoValido(Posicion destino, const Tablero& tablero)const override;
+};
+
+
